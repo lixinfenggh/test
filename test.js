43f26462bb8b79448feaf10e3f39430024a2bdb3
lixinfeng <lixinfeng@mq-ai.cn>
2022-09-02 03:09:34 +0000

    clean

--- test.js
+++
@@ -1,7 +0,0 @@
-const { execSync } = require("child_process");
-
-const myRepo = 'git@gitlab.mq-ai.cn:lixinfeng/test.git';
-const myRepoBranch = 'dev';
-const githubRepo = 'git@github.com:lixinfenggh/test.git';
-const githubBranch = 'main';
-
